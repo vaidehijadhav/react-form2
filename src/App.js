5bc0250d94e5843018ba265b730e3c8bdf5faf30
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-03-16 00:48:30 +0530

    create input for name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData]= useState({
+    firstName: ""
+  })
+
+  function changeHandler(event){
+    const [name, value, checked, type]= event.target;
+
+    setFormData((prev)=>({...prev, [name]: value}));
+  }
   return (
-    <h1 className='text-7xl text-center'>
-      hello world
-    </h1>
+    <div className='flex flex-col items-center mt-2'>
+      <form>
+        <input type='text' placeholder='vaidehi'
+        name='firstName' id='firstName' value={formData.firstName}
+        onChange={changeHandler}/>
+      </form>
+    </div>
   );
 }
 
