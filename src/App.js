1ece856139e1abcf0b5672e35a6a927b12ff942f
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-03-16 13:54:34 +0530

    create till checkboxes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,137 @@ import './App.css';
 
 function App() {
   const [formData, setFormData]= useState({
-    firstName: ""
+    firstName: "",
+    lastName: "",
+    email: "",
+    country: "India",
+    streetAddress: "",
+    city: "",
+    state: "",
+    postalCode: "", 
+    comments: false, candidates: false, offers: false
   })
 
   function changeHandler(event){
-    const [name, value, checked, type]= event.target;
+    const {name, value, checked, type} = event.target;
 
-    setFormData((prev)=>({...prev, [name]: value}));
+    setFormData(prev => ({
+      ...prev, 
+      [name]: type === "checkbox" ? checked : value,
+  }));
+
   }
   return (
     <div className='flex flex-col items-center mt-2'>
       <form>
+        <label htmlFor='firstName'>First Name</label>
+        <br/>
         <input type='text' placeholder='vaidehi'
         name='firstName' id='firstName' value={formData.firstName}
-        onChange={changeHandler}/>
+        onChange={changeHandler}
+        className='outline'/>
+
+        <br/>
+
+        <label htmlFor='lastName'>Last Name</label>
+        <br/>
+        <input type='text' placeholder='Jadhav'
+        name='lastName' id='lastName' value={formData.lastName}
+        onChange={changeHandler}
+        className='outline'/>
+
+        <br/>
+
+        <label htmlFor='email'>Email Address</label>
+        <br/>
+        <input type='text' placeholder='vaidehi@gmail.com'
+        name='email' id='email' value={formData.email}
+        onChange={changeHandler}
+        className='outline'/>
+
+        <br/>
+        <label htmlFor='country'>Country</label>
+        <br/>
+        <select id='country'
+        name='country' value={formData.country}
+        onChange={changeHandler}
+        className='outline'>
+
+            <option>India</option>
+            <option>USA</option>
+            <option>Canada</option>
+            <option>UK</option>
+        </select>
+
+        <br/>
+
+        <label htmlFor='streetAddress'>Street Address</label>
+        <br/>
+        <input type='text' placeholder='B-2c'
+        name='streetAddress' id='streetAddress' value={formData.streetAddress}
+        onChange={changeHandler}
+        className='outline'/>
+
+        <br/>
+
+        <label htmlFor='city'>City</label>
+        <br/>
+        <input type='text' placeholder='Pune'
+        name='city' id='city' value={formData.city}
+        onChange={changeHandler}
+        className='outline'/>
+
+        <br/>
+
+        <label htmlFor='state'>State / Province</label>
+        <br/>
+        <input type='text' placeholder='Maharashtra'
+        name='state' id='state' value={formData.state}
+        onChange={changeHandler}
+        className='outline'/>
+
+        <br/>
+
+    
+        <label htmlFor='postalCode'>ZIP / Postal Code</label>
+        <br/>
+        <input type='text' placeholder='415002'
+        name='postalCode' id='postalCode' value={formData.postalCode}
+        onChange={changeHandler}
+        className='outline'/>
+
+        <fieldset>
+          <legend>By Email</legend>
+          <div className='flex'>
+            <input id='comments' name='comments' type='checkbox' 
+            checked={formData.comments} onChange={changeHandler} />
+
+            <div>
+              <label htmlFor='comments'>Comments</label>
+              <p>Get notified when someonces posts a comment on a posting.</p>
+            </div>
+          </div>
+
+          <div className='flex'>
+            <input id='candidates' name='candidates' type='checkbox' 
+            checked={formData.candidates} onChange={changeHandler} />
+
+            <div>
+              <label htmlFor='candidates'>Candidates</label>
+              <p>Get notified when a  candidate applies for a job.</p>
+            </div>
+          </div>
+
+          <div className='flex'>
+            <input id='offers' name='offers' type='checkbox' 
+            checked={formData.offers} onChange={changeHandler} />
+
+            <div>
+              <label htmlFor='offers'>Offers</label>
+              <p>Get notified when a  candidate accepts or rejects an offer.</p>
+            </div>
+          </div>
+        </fieldset>
       </form>
     </div>
   );
